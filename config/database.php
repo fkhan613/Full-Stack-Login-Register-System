4aace0b366c989d6eb3a6e52536c83c54fbb227d
Farhan Khan <farhan.k2005@gmail.com>
2022-06-22 11:58:26 -0400

    added tab icon

--- config/database.php
+++ config/database.php
@@ -13,3 +13,13 @@ if(!$conn){
     die('Connection failed: '.mysqli_connect_error());
 } 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/jpg" href="../client/img/avatar.svg"/>
+</head>
+</html>
